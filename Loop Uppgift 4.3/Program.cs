f50f643c585dc1fd695133863d4d07661f3aea91
Anton <>
2023-03-03 09:12:14 +0100

    Add project files.

---
+++ Loop Uppgift 4.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 1; i <=100; i++)
+            {
+                if(i %3 == 0 && i%5 !=0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+                
+                   
+
+        }
+    }
+}
